79d87589de104e270bb2c83a484be98835879c0f
saikounonou5 <elnunez5@gmail.com>
2016-11-09 00:01:15 -0500

    Update nginx_install_config.py

--- nginx_install_config.py
+++ nginx_install_config.py
@@ -74,8 +74,7 @@ except ImportError:
      print 'pyparsing is not installed...'
      print 'Installing now...'
      print os.getcwd()
-     os.system('sudo mv ./pyparsing-*.rpm /')
-     os.system('sudo rpm -ivh pyparsing-*.rpm')
+     os.system('sudo rpm -ivh ./pyparsing-*.rpm')
     
 from nginxparser import load,dump,dumps
 import pyparsing
